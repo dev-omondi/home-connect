4a9ba1a6ddb8efca49f2def6faab7061d216182a
dev omondi <brianomondiomeno@gmail.com>
2026-03-16 21:56:50 +0300

    user router updated

--- back_end/routes/userRoute.js
+++ back_end/routes/userRoute.js
@@ -15,5 +15,7 @@ import { registerUser,
  router.route("/login").post(loginUser)
  router.route("/logout").post(logoutUser)
  router.route("/:id").put(protect,updateUser)
-
- export default router
+                    .delete(protect,authorizeUser("admin"),deleteUser)
+router.route("/").get(protect,authorizeUser("admin","manager"),getUsers)                  
+router.route("/updateRole/:id").put(protect,authorizeUser("admin"),updateRole)
+export default router
