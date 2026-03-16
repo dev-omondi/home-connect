bea31f3ca763e8617720aeee1d8b7d8b0eee3f0a
dev omondi <brianomondiomeno@gmail.com>
2026-03-16 19:13:00 +0300

    updated user controller

--- back_end/controllers/userController.js
+++ back_end/controllers/userController.js
@@ -99,3 +99,61 @@ const updateUser=asyncHandler(async(req,res)=>{
         _id:updatedUser._id
     })
 })
+//@descripton................................delete user
+//@api-----------------------------------------DELETE/api/user/:id
+//@access----------------------------------------private
+const deleteUser=asyncHandler(async(req,res)=>{
+    const user=await Users.findById(req.params._id)
+    if(!user){
+        res.status(404)
+        throw new Error("User not Found");
+        
+    }
+    if(req.user._id.toString()!==user._id.toString()&&
+    req.user.role!=="admin"){
+        res.status(403)
+        throw new Error("Forbidden,you cant delete another users account");    
+    }
+    await user.deleteOne()
+    res.status(200)
+    .json({message:"User account has been deleted succefully"})
+})
+
+//@description-------------------------------get users
+//@api---------------------------------------GET/api/user/:id
+//access----------------------------------------private
+const getUsers=asyncHandler(async(req,res)=>{
+    if(req.user.role!=="admin"&&req.user.role!=="manager"){
+        res.status(403)
+        throw new Error("Forbidden,you can not access this route")
+    }
+    const users=await Users.find()
+    res.status(200)
+    .json(users)
+})
+//@description------------------------------------update user role
+//@api------------------------------------------PUT/api/user/updateRole/:id
+//@access-------------------------------------------private
+
+const updateRole=asyncHandler(async(req,res)=>{
+    if(req.user.role!=="admin"){
+        res.status(403)
+        throw new Error("Unauthorised,can not update user roles")
+    }
+    const user=await Users.findById(req.params._id)
+    if (!user) {
+        res.status(404)
+        throw new Error("User not found")
+    }
+    user.role=req.body.role||user.role
+    const updatedUser=await user.save()
+    res.status(200).json(updateUser)
+})
+export{registerUser,
+    loginUser,
+    logoutUser,
+    updateUser,
+    deleteUser,
+    updateRole,
+    getUsers
+}
