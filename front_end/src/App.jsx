2f77e19b73f15f287ae189121f04a4e0a25f244f
dev omondi <brianomondiomeno@gmail.com>
2026-03-18 19:58:46 +0300

    front end readded

---
+++ front_end/src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='mt-30'>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
