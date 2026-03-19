a1ef0cda04109463225c4159db710794f8a3a10a
dev omondi <brianomondiomeno@gmail.com>
2026-03-19 13:25:10 +0300

    layout added

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,11 +1,21 @@
 
 import React from 'react'
 import { Button } from './components/ui/button'
-
+import Layout from './Layouts/Layout'
+import Homepage from './pages/Homepage'
+import Registerpage from './pages/Registerpage'
+import Loginpage from './pages/Loginpage'
+import{Route,Routes} from "react-router-dom"
 const App = () => {
   return (
     <div className='mt-30'>
-      <Button/>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Homepage/>}/>
+          <Route path='/login' element={<Loginpage/>}/>
+          <Route path='/register' element={<Registerpage/>}/>
+        </Route>
+      </Routes>
     </div>
   )
 }
---
+++ front_end/src/pages/Registerpage.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Registerpage = () => {
+  return (
+    <div>Registerpage</div>
+  )
+}
+
+export default Registerpage
