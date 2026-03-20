a5ec4ef469c52b685761cf1cadb74478bbfaa210
dev omondi <brianomondiomeno@gmail.com>
2026-03-20 19:21:12 +0300

    slices added

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import store from './store'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <Provider store={store} >
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>
   
 )
