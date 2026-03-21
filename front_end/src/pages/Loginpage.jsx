c0a1939a98ba55d8d504b2e9b9973eb2be41d33e
dev omondi <brianomondiomeno@gmail.com>
2026-03-21 20:36:34 +0300

    new changes

--- front_end/src/pages/Loginpage.jsx
+++ front_end/src/pages/Loginpage.jsx
@@ -1,9 +1,85 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+import{useNavigate} from "react-router-dom"
+import { useDispatch,useSelector } from 'react-redux'
+import { useLoginMutation } from '@/slices/baseUserApi'
+import { setCredentials } from '@/slices/authSlice'
 
 const Loginpage = () => {
+  const[email,setEmail]=useState("")
+  const[password,setPassword]=useState("")
+
+  const navigate=useNavigate()
+  const dispatch=useDispatch()
+
+  const userInfo=useSelector((state)=>state.auth.userInfo)
+  const [login,{isLoading}]=useLoginMutation()
+
+  useEffect(()=>{
+    if (userInfo) {
+      navigate("/")
+    }
+  },[userInfo,navigate])
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    try {
+      const res=await login({email,password}).unwrap()
+      dispatch(setCredentials({...res}))
+      navigate("/")
+    } catch (err) {
+      console.log(err?.data?.message||err.error)
+    }
+  }
   return (
-    <div>Loginpage</div>
+    <div className='w-full max-w-2xl mx-auto mt-8 '>
+      <Card className={"w-full rounded shadow-lg ring-0" }>
+      <CardHeader>
+        <CardTitle >get into your kejaConnect accout and enjoy our services</CardTitle>
+        <CardDescription>Enter your details below to login</CardDescription>
+        <CardAction>Sign In</CardAction>
+      </CardHeader>
+      <CardContent>
+        <form className='flex flex-col gap-y-4' id='login' onSubmit={handleSubmit}>
+          <div >
+            <Label>email</Label>
+            <Input
+            type={"email"}
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <Label>password</Label>
+            <Input
+            type={"password"}
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            />
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Button
+        type="submit"
+        form="login"
+        className={"mx-auto py-2 px-3 rounded hover:bg-green-700 cursor-pointer transition-all duration-200"}
+        >Sign In</Button>
+      </CardFooter>
+    </Card>
+    </div>
   )
 }
 
--- front_end/src/pages/Registerpage.jsx
+++ front_end/src/pages/Registerpage.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState,useEffect } from 'react'
+import { Eye,EyeOff } from 'lucide-react'
 import {
   Card,
   CardAction,
@@ -21,23 +22,26 @@ const Registerpage = () => {
   const[name,setName]=useState("")
   const[email,setEmail]=useState("")
   const[password,setPassword]=useState("")
+  const[togglePassword,setTogglePassword]=useState(false)
+  const[confirmPassword,setConfirmPassword]=useState("")
 
   const navigate=useNavigate()
   const dispatch=useDispatch()
-  const userInfor=useSelector((state)=>state.auth.userInfor)
+  const userInfo=useSelector((state)=>state.auth.userInfo)
   const [register,{isLoading}]=useRegisterMutation()
 
   useEffect(()=>{
-    if (userInfor) {
-      navigate("/")
+    if (userInfo) {
+      navigate("")
     }
-  },[userInfor,navigate])
+  },[userInfo,navigate])
 
   const hundeSubmit=async(e)=>{
     e.preventDefault()
     try {
       const res=await register({name,email,password}).unwrap()
       dispatch(setCredentials({...res}))
+      navigate("/")
       console.log("User created successfully")
     } catch (err) {
       console.log(err?.data?.message||err.error)
@@ -58,7 +62,7 @@ const Registerpage = () => {
             <div className='flex flex-col gap-2'>
             <Label>name</Label>
             <Input
-            className={"focus:ring-0 focus-border-0"}
+            className={"focus:ring-0 focus:border-0"}
               type={"text"}
               value={name}
               onChange={(e)=>setName(e.target.value)}
@@ -73,19 +77,49 @@ const Registerpage = () => {
                 onChange={(e)=>setEmail(e.target.value)}
               />
             </div>
-            <div className='flex flex-col gap-2 '>
+            <div className=' flex flex-col gap-2 '>
               <Label>password</Label>
-              <Input
-              className={"outline-none focus:outline-none"}
-              type={"password"}
+              <div className='relative'>
+               <Input
+              className={"outline-none py-0 focus:outline-none"}
+              type={togglePassword?"text":"password"}
               value={password}
               onChange={(e)=>setPassword(e.target.value)}
               />
+              <button className='absolute right-4 top-1/2 -translate-y-1/2 text-green-400'
+              type='button'
+              onClick={()=>setTogglePassword(!togglePassword)}
+              >
+                {togglePassword?<EyeOff/>:<Eye/>}
+              </button>
+              </div>
+            </div>
+            <div className='flex flex-col gap-2'>
+              <Label>Confirm Password</Label>
+              <div className='relative'>
+                <Input
+                type={togglePassword?"text":"password"}
+                value={password}
+                onChange={(e)=>setConfirmPassword(e.target.value)}
+                />
+                <button
+                className='absolute right-4 top-1/2 -translate-y-1/2'
+                type='button'
+                onClick={()=>setTogglePassword(!togglePassword)}
+                >
+                  {
+                    togglePassword?<EyeOff/>:<Eye/>
+                  }
+                </button>
+              </div>
             </div>
           </form>
         </CardContent>
         <CardFooter>
-         <Button className={"mx-auto rounded py-2 px-3 hover:cursor-pointer bg-green-400 transition duration-200"}>Sign Up</Button>
+         <Button className={"mx-auto rounded py-2 px-3 hover:cursor-pointer bg-green-400 transition duration-200"}
+         form="register"
+         type="submit"
+         >Sign Up</Button>
         </CardFooter>
       </Card>
     </div>
